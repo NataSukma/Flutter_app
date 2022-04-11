fbe6e01baf5c0b67af672a38f8c54fa5d5a0fb37
NataSukma <natasukma90@gmail.com>
2022-04-11 19:22:49 +0700

    Status

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -143,19 +143,22 @@
 //print(nilaiakhir3);
 //}
 
-import "dart:io";
+import 'dart:io';
 
 //contoh
 void main() {  
   for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= i; j++) {
-      stdout.("*");
+      stdout.write("*");
     }
     stdout.write(" \n");
   }
-}
+  stdout.write(" \n");
 
-//no 1
+  int a = 5;
+
+
+//bgian 1
 for (int b = 1; b <= a; b++) {
   for (int c = 4; c >= b; c--) {
     stdout.write(" ");
@@ -166,3 +169,30 @@ for (int b = 1; b <= a; b++) {
   stdout.write(" \n");
 }
 stdout.write(" \n");
+
+int n = 3;
+for (int a = 1; a < n; a++) {
+  for (int b = 2; b >= a; b--) {
+    stdout.write(" ");
+  }
+
+  for (int c = 1; c < a*2; c++) {
+    stdout.write("*");
+  }
+  stdout.write("\n");
+}
+//bagian 4
+for (int a = 3; a > 0; a--) {
+  for (int b = n; b > a; b--) {
+    stdout.write(" ");
+  }
+
+for (int c = 1; c < a*2; c++){
+  stdout.write("*");
+}
+stdout.write("\n");
+}
+
+
+
+}
